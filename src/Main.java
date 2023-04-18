d3b89d50570e3e3a8ec2ee66a8f08451072f2e9f
Galina-Sokolova <peraskevasokolova@gmail.com>
2023-04-18 20:39:20 +0300

    HW 4 added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        RedBlackTree treeRedBlack = new RedBlackTree();
+        treeRedBlack.add(3);
+        treeRedBlack.add(6);
+        treeRedBlack.add(1);
+        treeRedBlack.add(9);
+        treeRedBlack.add(5);
+        treeRedBlack.add(10);
+        treeRedBlack.add(7);
+        treeRedBlack.add(12);
     }
 }
